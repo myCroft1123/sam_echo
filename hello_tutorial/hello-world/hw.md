88d38ba0a6e7537895b47206b0ab53bcf7628d3b
Sean O'Dell <63874267+myCroft1123@users.noreply.github.com>
2020-12-15 14:44:35 -0800

    Add files via upload

---
+++ hello_tutorial/hello-world/hw.md
@@ -0,0 +1,7 @@
+write thin API around elasticsearch
+serverless rest API - post/get info (maybe update?) that will talk to elasticsearch
+
+- populate elasticsearch
+- query database
+- investigate manual requests against vpc mounted elasticsearch
+- may run into: if open access, authentication errors may occur. investigate
---
+++ hello_tutorial/ingest-url-extract/src/main/java/org/elasticsearch/plugin/ingest/url/extract/UrlExtractProcessor.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright [2020] [Sean]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.elasticsearch.plugin.ingest.url.extract;
+
+import org.elasticsearch.ingest.AbstractProcessor;
+import org.elasticsearch.ingest.IngestDocument;
+import org.elasticsearch.ingest.Processor;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static org.elasticsearch.ingest.ConfigurationUtils.readStringProperty;
+
+public class UrlExtractProcessor extends AbstractProcessor {
+
+    public static final String TYPE = "url_extract";
+
+    private final String field;
+    private final String targetField;
+
+    public UrlExtractProcessor(String tag, String description, String field,
+                 String targetField) throws IOException {
+        super(tag, description);
+        this.field = field;
+        this.targetField = targetField;
+    }
+
+    @Override
+    public IngestDocument execute(IngestDocument ingestDocument) throws Exception {
+        String content = ingestDocument.getFieldValue(field, String.class);
+        // TODO implement me!
+        ingestDocument.setFieldValue(targetField, content);
+        return ingestDocument;
+    }
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    public static final class Factory implements Processor.Factory {
+
+        @Override
+        public UrlExtractProcessor create(Map<String, Processor.Factory> factories, String tag,
+               String description, Map<String, Object> config) throws Exception {
+            String field = readStringProperty(TYPE, tag, config, "field");
+            String targetField = readStringProperty(TYPE, tag, config, "target_field", "default_field_name");
+
+            return new UrlExtractProcessor(tag, description, field, targetField);
+        }
+    }
+}
